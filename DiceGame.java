f2e9424d76a2e0a8b19d06b10cb2a0ae4ed329ce
s1250164 <40847569+s1250164@users.noreply.github.com>
2018-07-08 23:44:19 +0900

    Won / Lost message added
    Won / Lost message added

--- DiceGame.java
+++ DiceGame.java
@@ -8,11 +8,21 @@ class DiceGame{
   public int getSumDice(){
     return this.sumDice;
   }
+
   public int shakeDie(){
     int die;
     Random rand = new Random();
     die = rand.nextInt(6)+1;
     this.sumDice += die;
     return die;
+  }
+  public void judge(String name){
+    System.out.print(name);
+    if(getSumDice() >= 7){
+      System.out.println(" won!");
+    }
+    else{
+      System.out.println(" lose!");
+    }
   }
 }
--- Game.java
+++ Game.java
@@ -1,10 +1,15 @@
-class Game {
-  public static void main(String[] args){
+class Game{
+    public static void main(String[] args){
     DiceGame game = new DiceGame();
+    Greeting greeting = new Greeting();
+
+    greeting.greet();
 
     for(int i=0; i<2; i++){
       System.out.println("Die " + (i+1) + ": " + game.shakeDie());
     }
     System.out.println("Total value: " + game.getSumDice());
+
+    game.judge(greeting.getName());
   }
 }
